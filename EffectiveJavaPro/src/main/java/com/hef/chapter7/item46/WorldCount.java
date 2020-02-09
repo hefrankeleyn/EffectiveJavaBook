e1b0c4d9afcc3553b2ab94fbb0a795725b20f705
Li Fei <hefrankeleyn@gamil.com>
2020-02-09 16:06:37 +0800

    Stream 的使用
    Stream 收集器

--- EffectiveJavaPro/src/main/java/com/hef/chapter7/item46/WorldCount.java
+++ EffectiveJavaPro/src/main/java/com/hef/chapter7/item46/WorldCount.java
@@ -18,7 +18,9 @@ public class WorldCount {
     public static void main(String[] args) {
 //        keyToList();
 //        toMapTest01();
-        toMapTest02();
+//        toMapTest02();
+//        groupingByTest01();
+        joiningTest();
     }
 
     /**
@@ -85,6 +87,56 @@ public class WorldCount {
             System.out.printf("%s %s %d%n", entry.getKey(), ":", entry.getValue().getLen());
         }
 
+    }
+
+    private static void groupingByTest01(){
+        List<WorldObj> worldObjs = new ArrayList<>();
+
+        worldObjs.add(new WorldObj("aa", 2));
+        worldObjs.add(new WorldObj("aa", 3));
+        worldObjs.add(new WorldObj("bb", 1));
+        worldObjs.add(new WorldObj("aa", 23));
+
+        Map<String, List<WorldObj>> map = worldObjs.stream().collect(groupingBy(WorldObj::getWorld));
+        for (Map.Entry<String, List<WorldObj>> entry : map.entrySet()) {
+            System.out.printf("%s : %d%n", entry.getKey(), entry.getValue().size());
+        }
+        // 添加一个下游收集器
+        Map<String, Integer> map02 = worldObjs.stream().collect(groupingBy(WorldObj::getWorld, summingInt(WorldObj::getLen)));
+        for (Map.Entry<String, Integer> entry : map02.entrySet()) {
+            System.out.printf("%s %s %d%n", entry.getKey(), ":", entry.getValue());
+        }
+
+        worldObjs.stream().collect(groupingBy(WorldObj::getWorld,toCollection(LinkedHashSet::new)));
+
+        Map<String, Long> map03 = worldObjs.stream().collect(groupingBy(WorldObj::getWorld,counting()));
+
+
+        TreeMap<String, Long> treeMap = worldObjs.stream().collect(groupingBy(WorldObj::getWorld, TreeMap::new, counting()));
+
+        Map<Boolean, List<WorldObj>> map04 = worldObjs.stream().collect(partitioningBy(worldObj -> worldObj.getLen() > 10));
+        for (Map.Entry<Boolean, List<WorldObj>> entry : map04.entrySet()) {
+            System.out.printf("%s %d%n", entry.getKey(), entry.getValue().size());
+        }
+
+        Map<Boolean, Long> map05 = worldObjs.stream().collect(partitioningBy(worldObj -> worldObj.getLen() > 10, counting()));
+        for (Map.Entry<Boolean, Long> entry : map05.entrySet()) {
+            System.out.printf("%s %s %d%n", entry.getKey(), ":", entry.getValue());
+        }
+
+        long count = worldObjs.stream().count();
+
+
+    }
+
+
+    private static void joiningTest(){
+        String[] strArr = {"world", "hello"};
+        String str01 = Stream.of(strArr).collect(joining());
+        String str02 = Stream.of(strArr).collect(joining("$"));
+        String str03 = Stream.of(strArr).collect(joining("#", "{", "}"));
+        System.out.println(str01);
+        System.out.println(str03);
     }
 
     private static class WorldObj{
